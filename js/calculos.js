1233fff7493c900e7c7573c1c3d44ac67ec17d74
Macisenior <jlizqui@telefonica.net>
2026-07-26 23:38:34 +0200

    mejoras y detalles en saldos

--- js/calculos.js
+++ js/calculos.js
@@ -6,13 +6,30 @@ export function calcularGastoPorPersona(personas, gastos) {
   gastos.forEach(g => {
     if (!g.participantes || !g.monto) return;
 
-    const parte = g.monto / g.participantes.length;
-
     g.participantes.forEach(id => {
       if (!gastoPersona[id]) gastoPersona[id] = 0;
-      gastoPersona[id] += parte;
+      gastoPersona[id] += calcularImportePersonaEnGasto(g, id);
     });
   });
 
   return gastoPersona;
+}
+export function calcularImportePersonaEnGasto(gasto, personaId) {
+
+  if (!gasto.participantes?.includes(personaId)) return 0;
+
+  // Reparto clásico si no hay consumiciones
+  if (!gasto.consumiciones) {
+    return gasto.monto / gasto.participantes.length;
+  }
+
+  let totalPesos = 0;
+
+  gasto.participantes.forEach(id => {
+    totalPesos += gasto.consumiciones[id] || 1;
+  });
+
+  const miPeso = gasto.consumiciones[personaId] || 1;
+
+  return gasto.monto * miPeso / totalPesos;
 }
